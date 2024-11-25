577ac96754d994112aa957f25c435d639bf808b6
John Arocha <john.arocha@clover.com>
2024-11-25 11:42:35 -0700

    fixed tests to new endpoint

--- server/index.test.js
+++ server/index.test.js
@@ -13,23 +13,23 @@ describe("Items", () => {
         execSync("npm run seed");
     });
 
-    it("GET /api/items should return a status code of 200", async () => {
+    it("GET /items should return a status code of 200", async () => {
         const response = await request(app).get("/api/items");
         expect(response.statusCode).toBe(200);
     });
 
-    it("GET /api/items should return an array", async () => {
-        const response = await request(app).get("/api/items");
+    it("GET /items should return an array", async () => {
+        const response = await request(app).get("/items");
         expect(response.body).toBeInstanceOf(Array);
     })
 
-    it("GET /api/items should return correct number of restaurants", async () => {
-        const response = await request(app).get("/api/items");
+    it("GET /items should return correct number of restaurants", async () => {
+        const response = await request(app).get("/items");
         expect(response.body).toHaveLength(20);
     });
     
-    it("GET /api/items should return the correct data", async () => {
-        const response = await request(app).get("/api/items");
+    it("GET /items should return the correct data", async () => {
+        const response = await request(app).get("/items");
         expect(response.body[0]).toEqual(expect.objectContaining({
             "name":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
             "price":109.95,
@@ -39,8 +39,8 @@ describe("Items", () => {
          }));
     })
 
-    it("GET /api/items/:id should return a correct data for single item",  async() => {
-        const response = await request(app).get("/api/items/3");
+    it("GET /items/:id should return a correct data for single item",  async() => {
+        const response = await request(app).get("/items/3");
         expect(response.body).toEqual(expect.objectContaining({
             "name":"Mens Cotton Jacket",
             "price":55.99,
