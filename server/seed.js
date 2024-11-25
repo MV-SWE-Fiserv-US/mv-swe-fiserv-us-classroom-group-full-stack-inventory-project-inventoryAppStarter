5ae0df3b2591a6bf492b7c6486810a571365d342
Antony Perez <antonyperez0@yahoo.com>
2024-11-25 10:11:22 -0800

    deleted old lines

--- server/seed.js
+++ server/seed.js
@@ -1,7 +1,4 @@
-const { sauces, items } = require("./seedData.js");
 
-const { sequelize } = require("./db");
-const { Item } = require("./models");
 const {sequelize} = require('./db');
 const {Sauce} = require('./models');
 const {Item} = require('./models/Item.js')
