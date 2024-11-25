500aad0c44c3354960f7963f9af365b166e9bf26
John Arocha <john.arocha@clover.com>
2024-11-25 12:38:48 -0700

    added styling and changed endpoint of api.js

---
+++ public/react/components/ItemCard/ItemCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './ItemCard.css'
+
+export default function ItemCard({ items }) {
+    return (
+        <div className="item-container">
+            {items.map((item) => (
+            <div className="item-card" key={item.id}>
+                <h2>{item.name}</h2>
+                <p>{item.description}</p>
+                <p><span style={{fontWeight: 'bold'}} >Category:</span> {item.category}</p>
+                <p><span style={{fontWeight: 'bold'}} >Price:</span> ${item.price}</p>
+                <img src={item.image} alt={item.name} />
+            </div>
+            ))}
+        </div>
+    );
+}
