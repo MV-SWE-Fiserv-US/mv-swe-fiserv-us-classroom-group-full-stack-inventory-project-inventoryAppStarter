ebeb30be82d610c7982d3b03d86dff7ae000e930
David Kwon <david.kwon@clover.com>
2024-11-25 09:59:58 -0700

    Create Sequelize model for Item

--- server/models/index.js
+++ server/models/index.js
@@ -1,10 +1,17 @@
 const {Sequelize} = require('sequelize')
-const {sequelize} = require('../db')
+const { sequelize, DataTypes } = require('../db')
 
 const Sauce = sequelize.define("sauces", {
   name: Sequelize.STRING,
   image: Sequelize.STRING,
 });
+
+const Item = sequelize.define("items", {
+  name: DataTypes.STRING,
+  description: DataTypes.STRING,
+  price: DataTypes.FLOAT,
+  image: DataTypes.STRING
+})
 
 module.exports = {
   db: sequelize,
