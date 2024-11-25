b634ca43c61a45b5fb8ecd32e4f5dffb9db14c2f
John Arocha <john.arocha@clover.com>
2024-11-25 13:41:10 -0700

    dded comments

--- public/react/components/ItemCard/ItemCard.jsx
+++ public/react/components/ItemCard/ItemCard.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './ItemCard.css'
 
-export default function ItemCard({ items }) {
+export default function ItemCard({ items, setSingleItem }) {
+
+    function handleClick(item) {
+        setSingleItem(item)
+    }
+
     return (
-        <div className="item-container">
+        <div className="item-container" >
             {items.map((item) => (
-            <div className="item-card" key={item.id}>
+            <div className="item-card" key={item.id} onClick={() => handleClick(item)} >
                 <h2>{item.name}</h2>
                 <p><span style={{fontWeight: 'bold'}} >Category:</span> {item.category}</p>
                 <p><span style={{fontWeight: 'bold'}} >Price:</span> ${item.price}</p>
