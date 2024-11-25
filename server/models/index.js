007fbe9cadda5adcf0a947bdde46c16f47e202d3
Codey Gallup <codey.gallup@gmail.com>
2024-11-25 11:06:51 -0700

    Adding associations for models

--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require("sequelize");
 const { sequelize, DataTypes } = require("../db");
+const User = require("./User");
+const Order = require("./Order");
 
 const Sauce = sequelize.define("sauces", {
   name: Sequelize.STRING,
@@ -14,9 +16,17 @@ const Item = sequelize.define("items", {
   image: DataTypes.STRING,
 });
 
+User.hasMany(Order);
+Order.belongsTo(User);
+
+Order.belongsToMany(Item, { through: "Order Items" });
+Item.belongsToMany(Order, { through: "Order Items" });
+
 module.exports = {
   db: sequelize,
   Sauce,
   Item,
+  User,
+  Order,
   DataTypes,
 };
