959cf635e51fbb4c8a17f15d2260e32bc4eded8e
Mohamed Rafeek <mohamed.rafeek@mohamed.rafeek-MacBook-Pro>
2024-11-25 16:45:01 -0500

    added async function to deleteItem, created deletebutton component

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { ItemsList } from "./ItemsList";
+import { Item } from "./Item";
 import apiURL from "../api";
+import { DeleteButton } from "./DeleteButton";
 
 export const App = () => {
   const [items, setItems] = useState([]);
+  const [selectedItem, setSelectedItem] = useState(null); // New state
 
   async function fetchItems() {
     try {
@@ -15,15 +18,48 @@ export const App = () => {
     }
   }
 
+  async function deleteItem (itemToDelete) {
+    try{
+      const response = await fetch(`${apiURL}/items/${itemToDelete}`, {
+        method: "DELETE",
+      });
+      setSelectedItem(null)
+      
+    }catch(err){
+      console.log("error deleting item", err)
+    }
+  }
+
   useEffect(() => {
     fetchItems();
-  }, []);
+  }, [selectedItem]); //selectedItem added to dependancy
+  //deleteItem sets selectedItem to null which will cause useEffect to trigger to update the list 
+  //ternary will display list of items when selectedItem is null as well.
+
+  const handleSelectItem = (item) => {
+    setSelectedItem(item);
+  };
+
+  const handleBack = () => {
+    setSelectedItem(null);
+  };
 
   return (
     <main>
       <h1>Item Store</h1>
-      <h2>All Items</h2>
-      <ItemsList items={items} />
+      {selectedItem ? (
+        <div>
+          <button onClick={handleBack}>Back to Items</button>
+          <Item item={selectedItem} />
+          <DeleteButton deleteItem={deleteItem} item={selectedItem}/>
+        </div>
+      ) : (
+        <>
+          <h2>All Items</h2>
+          <ItemsList items={items} onSelectItem={handleSelectItem} />
+        </>
+      )}
     </main>
   );
 };
+
