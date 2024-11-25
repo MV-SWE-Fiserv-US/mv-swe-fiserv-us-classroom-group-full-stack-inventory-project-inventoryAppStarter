00b161cd39f3f0c9e5acf6f8efb395f697aa7edd
David Kwon <david.kwon@clover.com>
2024-11-25 15:08:32 -0700

    Implement controlled inputs and POST functionality with AddItemForm component

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -5,6 +5,7 @@ import apiURL from '../api'
 import ItemCard from './ItemCard/ItemCard'
 import ItemDescription from './ItemDescription/ItemDescription'
 
+
 export const App = () => {
 
 	const [items, setItems] = useState([])
@@ -14,7 +15,6 @@ export const App = () => {
 		try {
 			const response = await fetch(`${apiURL}/items`)
 			const itemsData = await response.json()
-			
 			setItems(itemsData)
 		} catch (err) {
 			console.log("Oh no an error! ", err)
@@ -28,7 +28,7 @@ export const App = () => {
 	return (
 		<main className="h-screen w-screen">	
       		<h1 style={{ fontSize : '5vw', justifySelf : 'center', paddingBottom : '40px' }} >Vaultry</h1>
-			{singleItem ? <ItemDescription singleItem={singleItem} /> : <ItemCard items={items} setSingleItem={setSingleItem} />} {/* // Change tis turnary to show the detail page once its created, when singleItem state is updated  */}
+			{singleItem ? <ItemDescription singleItem={singleItem} /> : <ItemCard items={items} setSingleItem={setSingleItem} />} 
 		</main>
 	)
 }
