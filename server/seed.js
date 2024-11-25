3e5788230eca35a5fe8e2e781d626705f8a15618
Antony Perez <antonyperez0@yahoo.com>
2024-11-25 10:12:14 -0800

    removed lines

--- server/seed.js
+++ server/seed.js
@@ -1,24 +1,18 @@
+const { sauces, items } = require("./seedData.js");
 
-const {sequelize} = require('./db');
-const {Sauce} = require('./models');
-const {Item} = require('./models/Item.js')
+const { sequelize } = require("./db");
+const { Sauce } = require("./models");
+const { Item } = require("./models/Item.js");
 
 const seed = async () => {
-
-    try {
-        // drop and recreate tables per model definitions
-        await sequelize.sync({ force: true });
-    
-        // insert data
-       // await Promise.all(sauces.map(sauce => Sauce.create(sauce)));
-
-        await Item.bulkCreate(items)
   try {
     // drop and recreate tables per model definitions
     await sequelize.sync({ force: true });
 
     // insert data
-    await Promise.all(items.map((item) => Item.create(item)));
+    // await Promise.all(sauces.map(sauce => Sauce.create(sauce)));
+
+    await Item.bulkCreate(items);
 
     console.log("db populated!");
   } catch (error) {
