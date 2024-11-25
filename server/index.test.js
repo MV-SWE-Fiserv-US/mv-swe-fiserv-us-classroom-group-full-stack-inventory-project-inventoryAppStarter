d0089ae016d915dc46500201e1357c75e833eed2
John Arocha <john.arocha@clover.com>
2024-11-25 11:48:10 -0700

    added POST test

--- server/index.test.js
+++ server/index.test.js
@@ -13,22 +13,22 @@ describe("Items", () => {
         execSync("npm run seed");
     });
 
-    it("GET /items should return a status code of 200", async () => {
-        const response = await request(app).get("/api/items");
+    test("GET /items should return a status code of 200", async () => {
+        const response = await request(app).get("/items");
         expect(response.statusCode).toBe(200);
     });
 
-    it("GET /items should return an array", async () => {
+    test("GET /items should return an array", async () => {
         const response = await request(app).get("/items");
         expect(response.body).toBeInstanceOf(Array);
     })
 
-    it("GET /items should return correct number of restaurants", async () => {
+    test("GET /items should return correct number of restaurants", async () => {
         const response = await request(app).get("/items");
         expect(response.body).toHaveLength(20);
     });
     
-    it("GET /items should return the correct data", async () => {
+    test("GET /items should return the correct data", async () => {
         const response = await request(app).get("/items");
         expect(response.body[0]).toEqual(expect.objectContaining({
             "name":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
@@ -39,7 +39,7 @@ describe("Items", () => {
          }));
     })
 
-    it("GET /items/:id should return a correct data for single item",  async() => {
+    test("GET /items/:id should return a correct data for single item",  async() => {
         const response = await request(app).get("/items/3");
         expect(response.body).toEqual(expect.objectContaining({
             "name":"Mens Cotton Jacket",
@@ -48,5 +48,19 @@ describe("Items", () => {
             "category":"men's clothing",
             "image":"https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg"
         }))
+    })
+
+    test("POST /items/:id should return a correct data for single item",  async() => {
+        const body = {
+            "name": "Sample Item",
+            "description": "This is a sample item description.",
+            "category": "Sample Category",
+            "price": 19.99,
+            "image": "http://example.com/sample-image.jpg"
+          }
+        const response = await request(app).post("/items/").send(body)
+        expect(response.status).toBe(201);
+        expect(response.body).toMatchObject(body);
+        expect(response.body).toMatchObject(body);
     })
 })
