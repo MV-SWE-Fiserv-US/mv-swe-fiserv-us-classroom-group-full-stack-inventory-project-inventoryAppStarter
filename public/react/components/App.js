a2897bde4100189fbcafe78e89fceb31903ce15a
John Arocha <john.arocha@clover.com>
2024-11-25 12:49:29 -0700

    cleaned up code

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,36 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import { SaucesList } from './SaucesList';
+import React, { useState, useEffect } from 'react'
+import { SaucesList } from './SaucesList'
 
 // import and prepend the api url to any fetch calls
-import apiURL from '../api';
-import ItemsList from './ItemsList';
-import ItemDetails from './ItemDetails';
+import apiURL from '../api'
+import ItemsList from './ItemsList'
+import ItemDetails from './ItemDetails'
 import ItemCard from './ItemCard/ItemCard'
 
 export const App = () => {
 
-	const [items, setItems] = useState([]);
+	const [items, setItems] = useState([])
 	const [singleItem, setSingleItem] = useState({
 		name: "",
 		description: "",
 		price: 0,
 		image: ""
-	});
+	})
 
 	async function fetchItems(){
 		try {
-			const response = await fetch(`${apiURL}/items`);
-			const itemsData = await response.json();
+			const response = await fetch(`${apiURL}/items`)
+			const itemsData = await response.json()
 			
-			setItems(itemsData);
+			setItems(itemsData)
 		} catch (err) {
 			console.log("Oh no an error! ", err)
 		}
 	}
 
 	useEffect(() => {
-		fetchItems();
-	}, []);
+		fetchItems()
+	}, [])
 
 	return (
 		<main>	
