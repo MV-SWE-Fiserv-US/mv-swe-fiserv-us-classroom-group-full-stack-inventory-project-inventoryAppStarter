f8f0eafb158d6c1e75c861a03cdf519f6fbcaff9
David Kwon <david.kwon@clover.com>
2024-11-26 15:08:15 -0700

    Add functionality to edit user info from Dashboard

--- public/react/components/Dashboard/Dashboard.jsx
+++ public/react/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Dashboard = () => {
+  const [disabledInputs, setDisabledInputs] = useState({
+    name: true,
+    email: true,
+    password: true,
+    submitter: true
+  });
+
+  const [userData, setUserData] = useState({
+    name: "Admin User",
+    email: "admin@example.com",
+    password: "adminpassword",
+    isAdmin: true,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+    setDisabledInputs({
+      ...disabledInputs,
+      submitter: false
+    })
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // editUser(formData);
+  };
+
+
   return (
     <section className="h-screen w-full flex justify-center py-12 px-12">
       <div className="w-full rounded-lg shadow-lg border border-gray-300 py-4 px-4">
@@ -12,41 +41,108 @@ const Dashboard = () => {
             Personal details and information.
           </p>
         </div>
-        <div className="mt-6 border-t border-gray-100">
-          <dl className="divide-y divide-gray-100">
-            <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-              <dt className="text-sm/6 font-medium text-gray-900">Full name</dt>
-              <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-                Dingus Dongus
-              </dd>
-            </div>
-            <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-              <dt className="text-sm/6 font-medium text-gray-900">
-                User Status:
-              </dt>
-              <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-                Admin/No?
-              </dd>
-            </div>
-            <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-              <dt className="text-sm/6 font-medium text-gray-900">
-                Email address
-              </dt>
-              <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-                margotfoster@example.com
-              </dd>
-            </div>
-            <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
-              <dt className="text-sm/6 font-medium text-gray-900">Password</dt>
-              <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
-                *********
-                <button className="ml-20 px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
-                  Change Password
-                </button>
-              </dd>
-            </div>
-          </dl>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div className="mt-6 border-t border-gray-100">
+            <dl className="divide-y divide-gray-100">
+              <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+                <dt className="text-sm/6 font-medium text-gray-900">
+                  Full name
+                </dt>
+                <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0 flex justify-between">
+                  <input
+                    type="text"
+                    name="name"
+                    value={userData.name}
+                    onChange={handleChange}
+                    disabled={disabledInputs.name}
+                    className="w-1/2"
+                  ></input>
+                  <button
+                    type="button"
+                    className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition w-1/4"
+                    onClick={() =>
+                      setDisabledInputs({
+                        ...disabledInputs,
+                        name: !disabledInputs.name,
+                      })
+                    }
+                  >
+                    Change Name
+                  </button>
+                </dd>
+              </div>
+              <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+                <dt className="text-sm/6 font-medium text-gray-900">
+                  User Status
+                </dt>
+                <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+                  {userData.isAdmin ? "Admin" : "Standard User"}
+                </dd>
+              </div>
+              <div className="bg-gray-50 px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+                <dt className="text-sm/6 font-medium text-gray-900">
+                  Email address
+                </dt>
+                <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0 flex justify-between">
+                  <input
+                    type="text"
+                    name="email"
+                    value={userData.email}
+                    onChange={handleChange}
+                    disabled={disabledInputs.email}
+                    className="w-1/2"
+                  ></input>
+                  <button
+                    type="button"
+                    className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition w-1/4"
+                    onClick={() =>
+                      setDisabledInputs({
+                        ...disabledInputs,
+                        email: !disabledInputs.email,
+                      })
+                    }
+                  >
+                    Change Email
+                  </button>
+                </dd>
+              </div>
+              <div className="bg-white px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-3">
+                <dt className="text-sm/6 font-medium text-gray-900">
+                  Password
+                </dt>
+                <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0 flex justify-between">
+                  <input
+                    type="text"
+                    name="password"
+                    value={userData.password.replace(/./g, "*")}
+                    onChange={handleChange}
+                    disabled={disabledInputs.password}
+                    className="w-1/2"
+                  ></input>
+                  <button
+                    type="button"
+                    className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition w-1/4"
+                    onClick={() =>
+                      setDisabledInputs({
+                        ...disabledInputs,
+                        password: !disabledInputs.password,
+                      })
+                    }
+                  >
+                    Change Password
+                  </button>
+                </dd>
+              </div>
+            </dl>
+          </div>
+          <button
+            type="submit"
+            className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition disabled:bg-gray-300 disabled:transition-none disabled:cursor-not-allowed"
+            disabled={disabledInputs.submitter}
+          >
+            Submit Changes
+          </button>
+        </form>
       </div>
     </section>
   );
