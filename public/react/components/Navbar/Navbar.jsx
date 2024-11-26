d8503bf656207b74b83f3607cb7cff30f1815711
David Kwon <david.kwon@clover.com>
2024-11-26 15:44:14 -0700

    Restructure Navbar brackets

--- public/react/components/Navbar/Navbar.jsx
+++ public/react/components/Navbar/Navbar.jsx
@@ -102,7 +102,7 @@ export default function Navbar({ setSingleItem }) {
                 </div>
             </div>
         </nav>
-    )
+    )}
             /* {isModalOpen && (
                 <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
                     <div className="bg-white p-6 rounded-lg shadow-lg">
