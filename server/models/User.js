7804db5bbaa2f587711131898575d5a0668f2a3e
Codey Gallup <codey.gallup@gmail.com>
2024-11-26 14:26:33 -0700

    Updating User model

--- server/models/User.js
+++ server/models/User.js
@@ -1,14 +1,14 @@
 const { sequelize, DataTypes } = require("../db");
 
 const User = sequelize.define("users", {
-  name: DataTypes.STRING,
-  email: DataTypes.STRING,
-  password: DataTypes.STRING,
+  name: { type: DataTypes.STRING, allowNull: false },
+  email: { type: DataTypes.STRING, allowNull: false, unique: true },
+  password: { type: DataTypes.STRING, allowNull: false },
   cart: {
     type: DataTypes.JSON,
     defaultValue: [],
   },
-  isAdmin: DataTypes.BOOLEAN,
+  isAdmin: { type: DataTypes.BOOLEAN, defaultValue: false },
 });
 
 module.exports = User;
