c80420a7f09c68af988e3f8d435f594dbd3ec9b9
David Kwon <david.kwon@clover.com>
2024-11-26 08:20:17 -0700

    Update spacing in ItemDescription component and add back button

--- public/react/components/ItemDescription/ItemDescription.jsx
+++ public/react/components/ItemDescription/ItemDescription.jsx
@@ -1,8 +1,15 @@
 export default function ItemDescription({ singleItem }) {
   return (
     <section className="max-w-4xl h-[70%] mx-auto bg-white shadow-2xl rounded-lg overflow-hidden border border-gray-200 flex">
-      <img src={singleItem.image} alt={singleItem.name} className="h-full w-1/2 object-contain" />
-      <div className="p-4 flex flex-col justify-around flex-wrap" >
+      <div className="w-1/2 h-full flex flex-col items-center just' p-4">
+      <a href="/" className="w-full">
+        <button className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
+          Add to Cart
+        </button>
+      </a>
+        <img src={singleItem.image} alt={singleItem.name} className="h-full w-1/2 object-contain" />
+      </div>
+      <div className="p-4 flex flex-col justify-around flex-wrap w-1/2" >
         <h2 className="text-xl font-semibold text-gray-800">{singleItem.name}</h2>
         <p className="text-gray-600 mt-2">{singleItem.description}</p>
         <div className="mt-4 flex justify-between items-center">
