c9e5444a6f021921e6ceea296d2e55fa8a0f0150
John Arocha <john.arocha@clover.com>
2024-11-27 13:49:32 -0700

    cleaned up code on Login.jsx

--- public/react/components/Login/Login.jsx
+++ public/react/components/Login/Login.jsx
@@ -100,142 +100,142 @@ export default function Login() {
 
   const loginPrompts = {
     login: (
-	<section className="h-screen w-full flex justify-center py-12 px-12">
-		<div className="w-full max-w-md rounded-lg shadow-lg border border-gray-300 py-2 px-4 max-h-[50vh] flex flex-col">
-			<div className="px-4 sm:px-0">
-				<h3 className="text-2xl font-semibold text-gray-900">
-					Login
-				</h3>
-				<p className="mt-1 max-w-2xl text-sm text-gray-500">
-					Please enter your credentials to login.
-				</p>
-			</div>
-			<form onSubmit={handleSubmit} className="flex flex-col mt-6 flex-grow">
-				<div className="mb-4">
-					<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email address</label>
-					<input
-					className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-					id="email"
-					type="email"
-					name="email"
-					value={loginFormData.email}
-					onChange={handleChange}
-					/>
+			<section className="h-screen w-full flex justify-center py-12 px-12">
+				<div className="w-full max-w-md rounded-lg shadow-lg border border-gray-300 py-2 px-4 max-h-[50vh] flex flex-col">
+					<div className="px-4 sm:px-0">
+						<h3 className="text-2xl font-semibold text-gray-900">
+							Login
+						</h3>
+						<p className="mt-1 max-w-2xl text-sm text-gray-500">
+							Please enter your credentials to login.
+						</p>
+					</div>
+					<form onSubmit={handleSubmit} className="flex flex-col mt-6 flex-grow">
+						<div className="mb-4">
+							<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email address</label>
+							<input
+							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+							id="email"
+							type="email"
+							name="email"
+							value={loginFormData.email}
+							onChange={handleChange}
+							/>
+						</div>
+						<div className="relative mb-6">
+							<input
+							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline pr-10"
+							id="password"
+							type={showPassword ? "text" : "password"}
+							name="password"
+							value={loginFormData.password}
+							onChange={handleChange}
+							/>
+							<button
+							type="button"
+							className="absolute inset-y-0 right-0 flex items-center px-3 py-2"
+							onClick={() => setShowPassword(!showPassword)}
+							>
+							<FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+							</button>
+						</div>
+						<div className="mt-auto flex space-x-4">
+							<button
+							className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition w-full"
+							onClick={() => setLoginOrRegister(null)}
+							>
+							Go Back
+							</button>
+							<button
+							type="submit"
+							className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition w-full"
+							>
+							Login
+							</button>
+						</div>
+					</form>
 				</div>
-				<div className="relative mb-6">
-					<input
-					className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline pr-10"
-					id="password"
-					type={showPassword ? "text" : "password"}
-					name="password"
-					value={loginFormData.password}
-					onChange={handleChange}
-					/>
-					<button
-					type="button"
-					className="absolute inset-y-0 right-0 flex items-center px-3 py-2"
-					onClick={() => setShowPassword(!showPassword)}
-					>
-					<FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
-					</button>
-				</div>
-				<div className="mt-auto flex space-x-4">
-					<button
-					className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition w-full"
-					onClick={() => setLoginOrRegister(null)}
-					>
-					Go Back
-					</button>
-					<button
-					type="submit"
-					className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition w-full"
-					>
-					Login
-					</button>
-				</div>
-			</form>
-		</div>
-    </section>),
+			</section>),
     register: (
-	<section className="h-screen w-full flex justify-center py-12 px-12">
-      <div className="w-full max-w-md rounded-lg shadow-lg border border-gray-300 py-2 px-4 max-h-[60vh] flex flex-col">
-        <div className="px-4 sm:px-0">
-			<h3 className="text-2xl font-semibold text-gray-900">
-				Register
-			</h3>
-			<p className="mt-1 max-w-2xl text-sm text-gray-500">
-				Please enter your details to register.
-			</p>
-        </div>
-        <form onSubmit={handleSubmitRegister} className="flex flex-col mt-6 flex-grow">
-          	<div className="mb-4">
-				<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">Full Name</label>
-				<input
-				className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-				id="name"
-				type="text"
-				name="name"
-				value={registerFormData.name}
-				onChange={handleChangeRegister}
-				/>
-          	</div>
-			<div className="mb-4">
-				<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email address</label>
-				<input
-				className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-				id="email"
-				type="email"
-				name="email"
-				value={registerFormData.email}
-				onChange={handleChangeRegister}
-				/>
-          	</div>
-			<div className="relative mb-6">
-				<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password</label>
-				<input
-					className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline pr-10"
-					id="password"
-					type={showPassword ? "text" : "password"}
-					name="password"
-					value={registerFormData.password}
-					onChange={handleChangeRegister}
-				/>
-				<button
-					type="button"
-					className="absolute top-1/2 right-0 transform translate-y-1 px-3"
-					onClick={() => setShowPassword(!showPassword)}
-				>
-				<FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
-				</button>
+			<section className="h-screen w-full flex justify-center py-12 px-12">
+			<div className="w-full max-w-md rounded-lg shadow-lg border border-gray-300 py-2 px-4 max-h-[60vh] flex flex-col">
+				<div className="px-4 sm:px-0">
+					<h3 className="text-2xl font-semibold text-gray-900">
+						Register
+					</h3>
+					<p className="mt-1 max-w-2xl text-sm text-gray-500">
+						Please enter your details to register.
+					</p>
+				</div>
+				<form onSubmit={handleSubmitRegister} className="flex flex-col mt-6 flex-grow">
+					<div className="mb-4">
+						<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">Full Name</label>
+						<input
+						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+						id="name"
+						type="text"
+						name="name"
+						value={registerFormData.name}
+						onChange={handleChangeRegister}
+						/>
+					</div>
+					<div className="mb-4">
+						<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">Email address</label>
+						<input
+						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+						id="email"
+						type="email"
+						name="email"
+						value={registerFormData.email}
+						onChange={handleChangeRegister}
+						/>
+					</div>
+					<div className="relative mb-6">
+						<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">Password</label>
+						<input
+							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline pr-10"
+							id="password"
+							type={showPassword ? "text" : "password"}
+							name="password"
+							value={registerFormData.password}
+							onChange={handleChangeRegister}
+						/>
+						<button
+							type="button"
+							className="absolute top-1/2 right-0 transform translate-y-1 px-3"
+							onClick={() => setShowPassword(!showPassword)}
+						>
+						<FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+						</button>
+					</div>
+					<div className={isVisible ? "" : "hidden"}>
+					<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="isAdmin">Admin</label>
+					<input
+						className="mr-2 leading-tight"
+						id="isAdmin"
+						type="checkbox"
+						name="isAdmin"
+						checked={registerFormData.isAdmin}
+						onChange={(e) => setRegisterFormData({ ...registerFormData, isAdmin: e.target.checked })}
+					/>
+					</div>
+					<div className="mt-auto flex space-x-4">
+						<button
+							className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition w-full"
+							onClick={() => setLoginOrRegister(null)}
+						>
+							Go Back
+						</button>
+						<button
+							type="submit"
+							className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition w-full"
+						>
+							Register
+						</button>
+					</div>
+				</form>
 			</div>
-          	<div className={isVisible ? "" : "hidden"}>
-            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="isAdmin">Admin</label>
-            <input
-              	className="mr-2 leading-tight"
-              	id="isAdmin"
-              	type="checkbox"
-              	name="isAdmin"
-              	checked={registerFormData.isAdmin}
-              	onChange={(e) => setRegisterFormData({ ...registerFormData, isAdmin: e.target.checked })}
-            />
-          	</div>
-          	<div className="mt-auto flex space-x-4">
-				<button
-					className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition w-full"
-					onClick={() => setLoginOrRegister(null)}
-				>
-					Go Back
-				</button>
-				<button
-					type="submit"
-					className="px-4 py-2 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition w-full"
-				>
-					Register
-				</button>
-          	</div>
-        </form>
-      </div>
-    </section>)
+			</section>)
   }
 
   return (
