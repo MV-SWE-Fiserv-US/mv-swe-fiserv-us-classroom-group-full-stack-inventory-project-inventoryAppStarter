6607e77542d6cac5685863b427ebcce2789ddf2a
John Arocha <john.arocha@clover.com>
2024-11-27 14:28:30 -0700

    cleaned up code in routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,41 +7,41 @@ const { User } = require("../models");
 const JWT_SECRET = process.env.JWT_SECRET || "super-secret-key";
 
 router.post("/register", async (req, res, next) => {
-  const { name, email, password, isAdmin } = req.body;
+    const { name, email, password, isAdmin } = req.body;
 
-  try {
-    const user = await User.create({ name, email, password, isAdmin });
-    res.status(201).json(user);
-  } catch (error) {
-    next(error);
-  }
+    try {
+        const user = await User.create({ name, email, password, isAdmin });
+        res.status(201).json(user);
+    } catch (error) {
+        next(error);
+    }
 });
 
 router.post("/login", async (req, res, next) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findOne({ where: { email } });
-    const passMatch = await bcrypt.compare(password, user.password);
-    if (!user || !passMatch) {
-      return res.status(400).json({ error: "Invalid email or password" });
-    }
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ where: { email } });
+        const passMatch = await bcrypt.compare(password, user.password);
+        if (!user || !passMatch) {
+            return res.status(400).json({ error: "Invalid email or password" });
+        }
 
-    const token = jwt.sign(
-      {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin,
-      },
-      JWT_SECRET,
-      {
-        expiresIn: "1h",
-      }
-    );
-    res.status(200).json({ token });
-  } catch (error) {
-    next(error);
-  }
+        const token = jwt.sign(
+            {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                isAdmin: user.isAdmin,
+            },
+            JWT_SECRET,
+            {
+                expiresIn: "1h",
+            }
+        );
+        res.status(200).json({ token });
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = router;
--- server/routes/items.js
+++ server/routes/items.js
@@ -5,101 +5,101 @@ const { Item } = require("../models");
 
 // GET /items
 router.get("/", async (req, res, next) => {
-  try {
-    const Items = await Item.findAll();
-    res.status(200).json(Items);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get("/:id", async (req, res, next) => {
-  const { id } = req.params;
-
-  try {
-    const targetItem = await Item.findByPk(id);
-    if (targetItem) {
-      res.status(200).json(targetItem);
-    } else {
-      res.status(404).json({ message: "Item not found" });
-    }
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.post(
-  "/",
-  [
-    check("name").notEmpty().withMessage("Name is required").trim(),
-    check("description").notEmpty().withMessage("Description is required").trim(),
-    check("category").notEmpty().withMessage("Category is required").trim(),
-    check("price").notEmpty().withMessage("Price is required").isFloat().withMessage("Price must be a number"),
-    check("image").notEmpty().withMessage("Image URL is required").trim(),
-  ],
-  async (req, res, next) => {
     try {
-      const errors = validationResult(req);
-
-      if (!errors.isEmpty()) {
-        console.log("Validation errors:", errors.array());
-        return res.status(400).json({ errors: errors.array() });
-      } else {
-        const createdItem = await Item.create(req.body);
-        return res.status(201).json(createdItem);
-      }
+        const Items = await Item.findAll();
+        res.status(200).json(Items);
     } catch (error) {
-      console.error("Error creating item:", error);
-      next(error);
+        next(error);
     }
-  }
-);
+    });
 
-router.put(
-  "/:id",
-  [
-    check("name").notEmpty().trim(),
-    check("description").notEmpty().trim(),
-    check("category").notEmpty().trim(),
-    check("price").notEmpty().isFloat(),
-    check("image").notEmpty().trim(),
-  ],
-  async (req, res, next) => {
+    router.get("/:id", async (req, res, next) => {
     const { id } = req.params;
 
     try {
-      const errors = validationResult(req);
-
-      if (!errors.isEmpty()) {
-        res.status(400).json({ errors: errors.array() });
-        return;
-      } else {
-        await Item.update(req.body, {
-          where: { id },
-        });
-        const updatedItem = await Item.findByPk(id);
-        res.status(202).json(updatedItem);
-      }
+        const targetItem = await Item.findByPk(id);
+        if (targetItem) {
+            res.status(200).json(targetItem);
+        } else {
+            res.status(404).json({ message: "Item not found" });
+        }
     } catch (error) {
-      next(error);
+        next(error);
     }
-  }
-);
+    });
+
+    router.post(
+        "/",
+        [
+            check("name").notEmpty().withMessage("Name is required").trim(),
+            check("description").notEmpty().withMessage("Description is required").trim(),
+            check("category").notEmpty().withMessage("Category is required").trim(),
+            check("price").notEmpty().withMessage("Price is required").isFloat().withMessage("Price must be a number"),
+            check("image").notEmpty().withMessage("Image URL is required").trim(),
+        ],
+        async (req, res, next) => {
+            try {
+                const errors = validationResult(req);
+
+                if (!errors.isEmpty()) {
+                    console.log("Validation errors:", errors.array());
+                    return res.status(400).json({ errors: errors.array() });
+                } else {
+                    const createdItem = await Item.create(req.body);
+                    return res.status(201).json(createdItem);
+                }
+            } catch (error) {
+                console.error("Error creating item:", error);
+                next(error);
+            }
+        }
+    );
+
+    router.put(
+        "/:id",
+        [
+            check("name").notEmpty().trim(),
+            check("description").notEmpty().trim(),
+            check("category").notEmpty().trim(),
+            check("price").notEmpty().isFloat(),
+            check("image").notEmpty().trim(),
+        ],
+        async (req, res, next) => {
+            const { id } = req.params;
+
+            try {
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                res.status(400).json({ errors: errors.array() });
+                return;
+            } else {
+                await Item.update(req.body, {
+                where: { id },
+                });
+                const updatedItem = await Item.findByPk(id);
+                res.status(202).json(updatedItem);
+            }
+            } catch (error) {
+                next(error);
+            }
+        }
+    );
 
 router.delete("/:id", async (req, res, next) => {
-  const { id } = req.params;
-  try {
-    const deletedItem = await Item.destroy({
-      where: { id },
-    });
-    if (deletedItem) {
-      res.status(203).send("Item deleted");
-    } else {
-      res.status(404).json({ message: "Item not found" });
+    const { id } = req.params;
+    try {
+        const deletedItem = await Item.destroy({
+            where: { id },
+        });
+        if (deletedItem) {
+            res.status(203).send("Item deleted");
+        } else {
+            res.status(404).json({ message: "Item not found" });
+        }
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    next(error);
-  }
 });
 
 module.exports = router;
