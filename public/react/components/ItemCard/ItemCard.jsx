9ac37ea2b01e31322ce73955979234f1b6f01b55
David Kwon <david.kwon@clover.com>
2024-11-27 12:23:40 -0700

    Implement react-toastify library for UI feedback upon successfully adding item to cart

--- public/react/components/ItemCard/ItemCard.jsx
+++ public/react/components/ItemCard/ItemCard.jsx
@@ -1,8 +1,10 @@
 import React, { useContext, useEffect, useState } from "react";
+import { toast, ToastContainer, Slide } from "react-toastify";
 import { useNavigate } from "react-router";
 import { AuthContext } from "../../AuthProvider";
 import { jwtDecode } from "jwt-decode";
 import apiURL from "../../api";
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function ItemCard({ items }) {
   const navigate = useNavigate();
@@ -11,17 +13,30 @@ export default function ItemCard({ items }) {
 
   async function addItemToCart(userId, itemId) {
     try {
-      const response = await fetch(`${apiURL}/users/${userId}/addToCart/${itemId}`, {
-        method: "PUT",
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      });
-      if(!response.ok) {
+      const response = await fetch(
+        `${apiURL}/users/${userId}/addToCart/${itemId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (!response.ok) {
         throw new Error("Item could not be added to your cart!");
       }
+      toast.success("Item added to cart 🛒", {
+        position: "top-center",
+        autoClose: 3000, //3 seconds
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        transition: Slide,
+      });
     } catch (err) {
-      console.log("Oh no an error! ", err)
+      console.log("Oh no an error! ", err);
+      toast.error("Item could not be added to cart 😰 Try again later.")
     }
   }
 
@@ -71,55 +86,62 @@ export default function ItemCard({ items }) {
         setUserId(decoded.id);
       }
     }
-  }, [])
-
+  }, []);
+  
   return (
-    <div className="bg-zinc-[#F8F7F5] font-black flex flex-wrap justify-evenly gap-4 mx-auto pb-12 px-4">
-      {items.map((item) => {
-        const num = Math.floor(Math.random() * 6);
-        return (
-          <div
+    <>
+      <div className="toast-container">
+        <ToastContainer />
+      </div>
+      <div className="bg-zinc-[#F8F7F5] font-black flex flex-wrap justify-evenly gap-4 mx-auto pb-12 px-4">
+        {items.map((item) => {
+          const num = Math.floor(Math.random() * 6);
+          return (
+            <div
             className="bg-white flex flex-col justify-between items-center w-80 h-[600px] rounded-lg border border-slate-200 shadow-lg"
             key={item.id}
-          >
-            <div className="h-1/2">
-              <img
-                className="bg-white p-8 rounded-t-lg  h-full aspect-auto object-contain cursor-pointer"
-                onClick={() => navigate(`/item/${item.id}`)}
-                src={item.image}
-                alt={item.name}
-              />
-            </div>
-            <div className="px-5 pb-5 bg-white mt-auto rounded-b-lg h-1/2 flex flex-col justify-end w-full">
-              <h5 className="bg-white text-lg font-semibold tracking-tight text-black  text-pretty">
-                {item.name}
-              </h5>
+            >
+              <div className="h-1/2">
+                <img
+                  className="bg-white p-8 rounded-t-lg  h-full aspect-auto object-contain cursor-pointer"
+                  onClick={() => navigate(`/item/${item.id}`)}
+                  src={item.image}
+                  alt={item.name}
+                />
+              </div>
+              <div className="px-5 pb-5 bg-white mt-auto rounded-b-lg h-1/2 flex flex-col justify-end w-full">
+                <h5 className="bg-white text-lg font-semibold tracking-tight text-black  text-pretty">
+                  {item.name}
+                </h5>
 
-              <div className="flex items-center justify-between mt-2.5 mb-5 bg-white">
-                <div className="flex items-center space-x-1 rtl:space-x-reverse bg-white">
-                  {generateStars(num)}
+                <div className="flex items-center justify-between mt-2.5 mb-5 bg-white">
+                  <div className="flex items-center space-x-1 rtl:space-x-reverse bg-white">
+                    {generateStars(num)}
+                  </div>
+                  <span className=" bg-white text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
+                    {num}.0
+                  </span>
                 </div>
-                <span className=" bg-white text-blue-800 text-xs font-semibold px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ms-3">
-                  {num}.0
-                </span>
-              </div>
-              <div className="flex items-center justify-between bg-white  mt-2 w-full">
-                <span className="bg-white text-xl font-bold text-black ">
-                  ${item.price.toFixed(2)}
-                </span>
-                <button
-                  type="button"
-                  disabled={!isLoggedIn}
-                  onClick={() => handleAddItem(item.id)}
-                  className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-gray-300 disabled:transition-none disabled:cursor-not-allowed"
-                >
-                  {isLoggedIn ? "Add to Cart" : "Login to Add to Cart"}
-                </button>
+                <div className="flex items-center justify-between bg-white  mt-2 w-full">
+                  <span className="bg-white text-xl font-bold text-black ">
+                    ${item.price.toFixed(2)}
+                  </span>
+                  <button
+                    type="button"
+                    disabled={!isLoggedIn}
+                    onClick={() => {
+                      handleAddItem(item.id);
+                    }}
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center disabled:bg-gray-300 disabled:transition-none disabled:cursor-not-allowed"
+                  >
+                    {isLoggedIn ? "Add to Cart" : "Login to Add to Cart"}
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
