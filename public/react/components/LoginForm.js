fd0a5a8b3354e5b735c1f70c5577568fc60d3c3e
med4h <adrian.meda6@gmail.com>
2024-12-03 10:24:06 -0800

    idk

--- public/react/components/LoginForm.js
+++ public/react/components/LoginForm.js
@@ -38,6 +38,7 @@ export default function LoginForm({ setUser, setViewLoginForm }) {
     <>
     <div>
       <form onSubmit={handleSubmit}>
+        <label>
             Username:
             <input 
                 className="userpassinput"
